081e853c38e7ee64dba28f9b032a45f8330bfa89
OlegT <oleg133485@gmail.com>
2024-11-02 11:48:45 +0300

    адаптив

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
  
 //  burger menu
- $(document).ready(function() {
-   $('.header').click(function() {
-     $('.header__links').toggleClass('active');
-     $('body').toggleClass('stop-skroll');
-   });
- });
+$(document).ready(function() {
+  $('.header__burger').click(function() {
+      $('.header__nav').toggleClass('active');
+      $('body').toggleClass('stop-scroll');
+
+      $(this).toggleClass('active');
+  });
+});
+
  
-// Получаем элементы ползунка и отображаемого значения
 const progressRange = document.getElementById('progressRange');
 const progressValue = document.getElementById('progressValue');
 
-// Обновляем текст при изменении значения ползунка
 progressRange.addEventListener('input', () => {
-    // Устанавливаем текстовое значение в зависимости от текущего значения ползунка
     const currentValue = progressRange.value;
     progressValue.textContent = `${currentValue}%`;
 
-    // Можно также изменить ширину заполнения для наглядного прогресс-бара
     progressRange.style.background = `linear-gradient(to right, #0099ff ${currentValue}%, #ddd ${currentValue}%)`;
 });
 const selectContainer = document.querySelector('.form__select');
 const selectTitle = selectContainer.querySelector('.form__select-title');
 const selectLabels = selectContainer.querySelectorAll('.form__select-label');
 
-// Toggle menu
 selectTitle.addEventListener('click', (e) => {
-  e.stopPropagation(); // Остановить всплытие, чтобы клик не дошел до document
+  e.stopPropagation(); 
   if (selectContainer.getAttribute('data-state') === 'active') {
     selectContainer.setAttribute('data-state', '');
   } else {
@@ -34,7 +32,6 @@ selectTitle.addEventListener('click', (e) => {
   }
 });
 
-// Close when clicking on an option
 selectLabels.forEach(label => {
   label.addEventListener('click', (event) => {
     selectTitle.textContent = event.target.textContent;
@@ -42,7 +39,6 @@ selectLabels.forEach(label => {
   });
 });
 
-// Close when clicking outside of the select container
 document.addEventListener('click', (e) => {
   if (!selectContainer.contains(e.target)) {
     selectContainer.setAttribute('data-state', '');
