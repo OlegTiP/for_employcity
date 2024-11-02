5ea161fe936b48f74384620678e20a8f97118fff
OlegT <oleg133485@gmail.com>
2024-11-02 10:22:32 +0300

    закончил десктоп

--- js/script.js
+++ js/script.js
@@ -7,3 +7,44 @@
    });
  });
  
+// Получаем элементы ползунка и отображаемого значения
+const progressRange = document.getElementById('progressRange');
+const progressValue = document.getElementById('progressValue');
+
+// Обновляем текст при изменении значения ползунка
+progressRange.addEventListener('input', () => {
+    // Устанавливаем текстовое значение в зависимости от текущего значения ползунка
+    const currentValue = progressRange.value;
+    progressValue.textContent = `${currentValue}%`;
+
+    // Можно также изменить ширину заполнения для наглядного прогресс-бара
+    progressRange.style.background = `linear-gradient(to right, #0099ff ${currentValue}%, #ddd ${currentValue}%)`;
+});
+const selectContainer = document.querySelector('.form__select');
+const selectTitle = selectContainer.querySelector('.form__select-title');
+const selectLabels = selectContainer.querySelectorAll('.form__select-label');
+
+// Toggle menu
+selectTitle.addEventListener('click', (e) => {
+  e.stopPropagation(); // Остановить всплытие, чтобы клик не дошел до document
+  if (selectContainer.getAttribute('data-state') === 'active') {
+    selectContainer.setAttribute('data-state', '');
+  } else {
+    selectContainer.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when clicking on an option
+selectLabels.forEach(label => {
+  label.addEventListener('click', (event) => {
+    selectTitle.textContent = event.target.textContent;
+    selectContainer.setAttribute('data-state', '');
+  });
+});
+
+// Close when clicking outside of the select container
+document.addEventListener('click', (e) => {
+  if (!selectContainer.contains(e.target)) {
+    selectContainer.setAttribute('data-state', '');
+  }
+});
